7c1559343aceeb47270f65d39ed8d5c9e2e2587e
Daniel Leal <os.leal90@gmail.com>
2020-04-15 10:45:29 -0300

    Finished backend

---
+++ config/routes.rb
@@ -0,0 +1,34 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  root 'home#index'
+
+  concern :favoritable do |options|
+    shallow do
+      post '/favorite', { to: "favorites#create", on: :member }.merge(options)
+      delete '/favorite',  { to: "favorites#destroy", on: :member }.merge(options)
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :dashboard, only: :index
+      resources :categories, only: [:index, :show]
+      resources :search, only: :index
+      resources :favorites, only: :index
+      resources :songs, only: [] do 
+        concerns :favoritable, favoritable_type: 'Song'
+      end
+
+      resources :albums, only: :show do
+        resources :recently_heards, only: :create
+        # concerns :favoritable, favoritable_type: 'Album'
+      end
+      
+    end
+  end
+
+
+
+
+end 
